8fd91c45e17c0bd28ba032309a13b297c82c3c87
Marvin <marvins.rocha@gmail.com>
2023-06-19 09:18:25 -0400

    update finalizado e validação simples ok

--- src/Pages/Form.tsx
+++ src/Pages/Form.tsx
@@ -4,10 +4,10 @@ import axios from 'axios'
 import '../Style/Form.css'
 
 function Form() {
-  const [login,setLogin]=useState('');
+  const [login,setLogin]=useState("");
   const [email,setEmail]=useState('');
-  const [password,setPassword]=useState();
-  const [update,setUpdate]=useState('');
+  const [password,setPassword]=useState('');
+  const [update,setUpdate]=useState("");
   const [validation_password,setValidation_Password]= useState('');
   const [userList,setUserList]= useState ([]);
 
@@ -18,33 +18,30 @@ function Form() {
   })
   },[]) 
 
-  /* const rules = {
-    usuario: [{ required: true, message: 'Informe seu usuário' }],
-    password:[{ required: true, message: 'Informe sua senha' },{ min: 6, message: 'A senha deve conter no minimo 6 caracteres' }]
-} */
-
   function onDelete(login: any){
     axios.delete(`http://localhost:3001/api/delete/${login}`)
   }
 
   function onUpdate(login: any){
-    axios.put("http://localhost:3001/api/update",{
-      
+    axios.put("http://localhost:3001/api/put",{
+      senha:update,
+      login:login,
     })
+    setUpdate("")
   }
 
   function onSubmit(){
     if(validation_password !== password){
       alert("Senhas diferentes pf corrija")
     }  
-    else if(login == ""){ 
-      alert("Campo nome vazio, por favor registre um nome")
+    else if(login == "" || login.length < 2){ 
+      alert("Campo nome vazio ou a baixo de 2 caracter, por favor registre um nome valido")
     }
     else if(email == ""){
       alert("Campo email vazio, por favor registre um email valido")
     }
-    else if(password == "" || password < 5){
-      alert("Campo invalido, por favor registre uma senha valida")
+    else if(password == "" || password.length < 5){
+      alert("Campo invalido, por favor registre uma senha valida com no minimo 6 caracteres")
     }
     else{
       axios.post("http://localhost:3001/api/post",{
@@ -56,7 +53,7 @@ function Form() {
     });
     setUserList([
       ...userList,
-      {login:login,email:email,senha:password},
+      {login:login,email:email,senha:password}, 
     ]);
     }
   }
@@ -89,9 +86,9 @@ function Form() {
           <h4>Login:</h4> {val.login} | 
           <h4>Email:</h4> {val.email} | 
           <h4 >Senha:</h4> {val.senha } 
-          <input style={{marginLeft:"5px"}} type="text" onChange={(e)=>setUpdate(e.target.value)}/>
-          <button className="button_map">Update</button>
-          <button className="button_map" onClick={()=> {onDelete(val.login)}}>Deletar</button>
+          <input style={{marginLeft:"5px"}} type="text" onChange={(e)=>{setUpdate(e.target.value)}}/>
+          <button type="submit" className="button_map" onClick={()=> {onUpdate(val.login)}}>Update</button>
+          <button type="submit" className="button_map" onClick={()=> {onDelete(val.login)}}>Deletar</button>
           
           </div>)
         })} 
