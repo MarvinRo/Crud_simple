6a1c1c14949381a63a8ffbb4c42214bb532d6edf
Marvin <marvins.rocha@gmail.com>
2023-06-24 14:26:10 -0400

    falta zerar o campo após o envio

--- src/Pages/Form.tsx
+++ src/Pages/Form.tsx
@@ -16,13 +16,9 @@ interface DataType {
   senha: string;
 }
 
-
-
-
-
 const Form: React.FC = () => {
-
   const [data, setData] = useState<DataType[]>([]);
+  const [selectedLogin, setSelectedLogin] = useState<string>(""); // Estado para armazenar o login selecionado
 
   const columns: ColumnsType<DataType> = [
     {
@@ -51,7 +47,7 @@ const Form: React.FC = () => {
       key: 'x',
       render: (text, record) => (
         <div style={{ display: 'flex' }}>
-          <ModalUpload />
+          <ModalUpload login={selectedLogin} fetchData={fetchData} /> {/* Passando o login selecionado e a função fetchData como propriedades */}
           <button
             style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}
             type="submit"
@@ -97,104 +93,18 @@ const Form: React.FC = () => {
   return (
     <div style={{ marginTop: '25vh', marginRight: 'auto', marginLeft: 'auto', width: '900px' }}>
       <ModalCadastro />
-      <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={{ pageSize: 50 }}
+        scroll={{ y: 240 }}
+        onRow={(record) => ({
+          onClick: () => setSelectedLogin(record.login), // Atualiza o login selecionado
+        })}
+        rowClassName={(record) => (record.login === selectedLogin ? 'selected-row' : '')} // Estiliza a linha selecionada
+      />
     </div>
   );
 };
 
-
-/*
-function Form() {
-  const [login,setLogin]=useState("");
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
-  const [update,setUpdate]=useState("");
-  const [validation_password,setValidation_Password]= useState('');
-  const [userList,setUserList]= useState ([]);
-
-
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/get').then((response) => {
-      setUserList(response.data)
-  })
-  },[]) 
-
-  function onDelete(login: any){
-    axios.delete(`http://localhost:3001/api/delete/${login}`)
-  }
-
-  function onUpdate(login: any){
-    
-    axios.put("http://localhost:3001/api/put",{
-      senha:update,
-      login:login,
-    })
-    setUpdate("")
-  }
-
-   function onSubmit(){
-    if(validation_password !== password){
-      alert("Senhas diferentes pf corrija")
-    }  
-    else if(login == "" || login.length < 2){ 
-      alert("Campo nome vazio ou a baixo de 2 caracter, por favor registre um nome valido")
-    }
-    else if(email == ""){
-      alert("Campo email vazio, por favor registre um email valido")
-    }
-    else if(password == "" || password.length < 5){
-      alert("Campo invalido, por favor registre uma senha valida com no minimo 6 caracteres")
-    }
-    else{
-      axios.post("http://localhost:3001/api/post",{
-      login:login,
-      email:email,
-      senha:password,
-    }).then(()=>{
-      alert("Success!");
-    });
-    setUserList([
-      ...userList,
-      {login:login,email:email,senha:password}, 
-    ]);
-    }
-  }
-  return (
-    <>
-    <div className='Form'>
-      <label>
-        Nome:
-        <input type="text" placeholder='Cadastre seu nome aqui'onChange={(e)=>{setLogin(e.target.value)}}/>
-      </label>
-      <label>
-        Email:
-        <input type="email" placeholder='Cadastre seu email aqui'onChange={(e)=>{setEmail(e.target.value)}}/>
-      </label>
-      <label>
-        Senha:
-        <input type="password" placeholder='Cadastre sua senha aqui'onChange={(e)=>{setPassword(e.target.value)}}/>
-      </label>
-      <label>
-        Confirme sua senha:
-        <input type="password" placeholder='Repita suasenha aqui'onChange={(e)=>{setValidation_Password(e.target.value)}}/>
-      </label>
-
-      <button onClick={onSubmit}>Cadastrar</button>
-
-    </div>
-    
-      {userList.map((val)=>{
-        return (<div className='UserList'>
-          <h4>Login:</h4> {val.login} | 
-          <h4>Email:</h4> {val.email} | 
-          <h4 >Senha:</h4> {val.senha } 
-          <input style={{marginLeft:"5px"}} type="text" onChange={(e)=>{setUpdate(e.target.value)}}/>
-          <button type="submit" className="button_map" onClick={()=> {onUpdate(val.login)}}>Update</button>
-          <button type="submit" className="button_map" onClick={()=> {onDelete(val.login)}}>Deletar</button>
-          
-          </div>)
-        })} 
-    </>
-  ) */
-
-export default Form
+export default Form;
--- src/Pages/ModalUpload.tsx
+++ src/Pages/ModalUpload.tsx
@@ -1,37 +1,44 @@
 import React, { useState } from 'react';
-import { Button, Modal ,Input } from 'antd';
+import { Button, Modal, Input } from 'antd';
 import axios from 'axios';
-import lapis from '../img/writing.png'
+import lapis from '../img/writing.png';
 
+interface ModalUploadProps {
+  login: string;
+  fetchData: () => void;
+}
 
-const ModalUpload: React.FC = () => {
+const ModalUpload: React.FC<ModalUploadProps> = ({ login, fetchData }) => {
   const [update, setUpdate] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [login, setLogin] = useState(""); // Adicionado estado para armazenar o valor do login
 
   const showModal = () => {
+    setUpdate("");
     setIsModalOpen(true);
   };
 
   const handleCancel = () => {
+    setUpdate("");
     setIsModalOpen(false);
   };
 
-  function onUpdate() {
+  const onUpdate = () => {
     axios
       .put("http://localhost:3001/api/put", {
         senha: update,
         login: login,
       })
       .then(response => {
-        setUpdate("");
         setIsModalOpen(false);
+        setUpdate("");
+        fetchData();
       })
       .catch(error => {
         console.error(error);
       });
+      
   }
-
+  
   return (
     <>
       <Button
@@ -42,10 +49,13 @@ const ModalUpload: React.FC = () => {
         <img src={lapis} alt="Lápis" />
       </Button>
       <Modal
-        title="Troca de senha" open={isModalOpen} onOk={onUpdate} okText='Mudar' onCancel={handleCancel} cancelText='Cancelar'
+        title="Troca de senha"
+        open={isModalOpen}
+        onOk={onUpdate}
+        okText='Mudar'
+        onCancel={handleCancel}
+        cancelText='Cancelar'
       >
-        <label>Login:</label>
-        <Input placeholder="Digite seu login" onChange={e => setLogin(e.target.value)} />
         <label>Senha:</label>
         <Input placeholder="Digite sua senha" onChange={e => setUpdate(e.target.value)} />
       </Modal>
